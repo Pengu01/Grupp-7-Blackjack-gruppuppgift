a858f4b0bcb3ab5c0851f7bc1253528df44a7110
Pengu <alfons.stoltz@gmail.com>
2021-10-21 13:54:16 +0200

    Update Program.cs

--- Blackjack- gruppuppgift/Program.cs
+++ Blackjack- gruppuppgift/Program.cs
@@ -6,7 +6,7 @@ namespace Blackjack__gruppuppgift
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello!");
         }
     }
 }
