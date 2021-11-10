10cd679158a988270a36fada4d5d5f8c8b22217d
Pengu <59419974+Pengu01@users.noreply.github.com>
2021-11-10 13:31:37 +0100

    fixar

--- Blackjack- gruppuppgift/Program.cs
+++ Blackjack- gruppuppgift/Program.cs
@@ -1,165 +1,200 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 
-namespace Blackjack__gruppuppgift
-
+namespace BlackJackGame
+{
     class Program
     {
         static void Main(string[] args)
         {
-            //Du får välja vad ess är värt
-            int ess;
-            Console.WriteLine("Hej och välkommen till Black Jack");
-            Console.Write("vänligen skriv in vad ess ska vara värt(1 eller 11):");
-            ess= Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("bra, ess är nu värt" + ess);
-            Console.WriteLine("då kör vi");
-            //spelare och bot får två kort
-            
-        }
-    }
-    
-    class AIcard
-    {
-        List<int> bot = new List<int>();
-        public void botdeck()
-        {
-            int cardvalue = 0;
-            int kortvärde = 0;
-            for (int botkort = 0; botkort < 52; botkort++)
+            blackjack bj = new blackjack();
+            while (true)
             {
-                for (kortvärde = 0; kortvärde < 16;)
+                Console.ResetColor();
+                Console.ReadKey();
+                Console.Clear();
+                bj.reset();
+                bj.start();
+                if (bj.checkBJ())
                 {
-                    kortvärde++;
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine("Blackjack ez win");
+                    continue;
                 }
-
-                if (cardvalue < 16)
+                bool drawing = true;
+                while (drawing)
                 {
-                    Console.WriteLine("jag tar ett kort");
+                    Console.WriteLine(bj.playerValue());
+                    if (!bj.checkPlayer())
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.WriteLine("Bust");
+                        break;
+                    }
+                    Console.WriteLine("Hit/Stand");
+                    string answer = Console.ReadLine();
+                    if (answer == "Hit")
+                    {
+                        bj.playerHit();
+                    }
+                    else if (answer == "Stand")
+                    {
+                        drawing = false;
+                    }
+                    else continue;
                 }
-
-                if ( kortvärde == 16 || kortvärde < 16)
+                if (!bj.checkPlayer())
                 {
-                    Console.WriteLine("Jag står över");
+                    continue;
+                }
+                bool dealer = true;
+                while (dealer)
+                {
+                    if (bj.dealerValue() < 17)
+                    {
+                        bj.dealerHit();
+                    }
+                    else dealer = false;
+                }
+                if (!bj.checkDealer())
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine("Dealer bust! ez win");
+                    continue;
+                }
+                if (bj.compare() == 0)
+                {
+                    Console.WriteLine("Tie");
+                }
+                else if (bj.compare() == 1)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine("Win");
+                }
+                else if (bj.compare() == 2)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("Loss");
                 }
             }
         }
     }
-    
-    class playercard
-    {
-        List<int> player = new List<int>();
-        public void playerdeck()
-        {
-
-        }
-    }
-    //E
     class deck
     {
+        Random rnd = new Random();
         List<int> cards = new List<int>();
-        public void deckAdd()
-        {
-            for (int i = 2; i <= 10; i++)
-            {
-                for (int j = 0; j < 4; j++)
-                {
-                    cards.Add(i);
-                }
-            }
-        }
-        public int draw()
-        {
-            Random rnd = new Random();
-            return cards[rnd.Next(0, cards.Count)];
-        }
-        public void deckRemove()
+        public void deckReset()
         {
             cards.Clear();
-        }
-        public void addEss(int ess)
-        {
-            for(int i = 0; i < 4; i++)
+            for (int i = 2; i < 12; i++)
             {
-                cards.Add(ess);
-            }
-        }
-    }
-}
-
- /*class Program
-    {
-        static void Main(string[] args)
-        {
-            const int GROUP_SIZE = 4;
-            const int GROUP_AMOUNT = 8;
-
-            string[] studentArray =
-             {
-             "Abdalaziz", "Johannes", "Eliyah", "Ebba", "Ossian", "Adam",
-             "Gabriel", "Ahmad", "Mukhtar", "Hugo", "Yayhe", "Alexander",
-             "Theo", "Albin", "John", "Leo", "Benjamin", "Erik", "Omar",
-             "Ajob", "Emil", "Jim", "Viggo", "Hassan", "Abd", "Noah",
-             "Sebastian", "Alfons", "Romeo", "Annie", "Gvidas", "Samir"
-            };
-            List<string> students = new List<string>();
-
-            for (int i = 0; i < studentArray.Length; i++)
-            {
-                students.Add(studentArray[i]);
-
-            }
-            Console.WriteLine("antal elever i listan " + students.Count);
-
-            Random random = new Random();
-            
-            for (int i = 1; i < GROUP_AMOUNT + 1; i++)
-            {
-                Console.WriteLine("group" + i + ":");
-
-                for (int j = 0; j < GROUP_SIZE; j++)
+                cards.Add(i);
+                if (i == 10)
                 {
-                    if (students.Count == 0)
+                    for (int k = 0; k < 12; k++)
                     {
-                        Console.WriteLine("Alla elever har fått en grupp");
-                        break;
-
+                        cards.Add(i);
                     }
-                    int localValue = random.Next(0, students.Count);
-                    Console.WriteLine(students[localValue]);
-
-                    students.RemoveAt(localValue);
-
                 }
-                Console.Write("\n");
             }
-            Console.WriteLine("tryck på valfri tangent för att lämna");
-            Console.ReadKey();
         }
-
-
+        public int deckDraw()
+        {
+            return cards[rnd.Next(0, cards.Count)];
+        }
+    }
+    class blackjack  : deck
+    {
+        List<int> player = new List<int>();
+        List<int> dealer = new List<int>();
+        public void playerHit()
+        {
+            player.Add(deckDraw());
+        }
+        public void dealerHit()
+        {
+            dealer.Add(deckDraw());
+        }
+        public void start()
+        {
+            for(int i = 0; i < 2; i++)
+            {
+                player.Add(deckDraw());
+                dealer.Add(deckDraw());
+            }
+        }
+        public bool checkDealer()
+        {
+            int value = dealerValue();
+            if (value > 21)
+            {
+                if (dealer.Remove(11))
+                {
+                    dealer.Add(1);
+                    return true;
+                }
+                return false;
+            }
+            return true;
+        }
+        public bool checkPlayer()
+        {
+            int value = playerValue();
+            if (value > 21)
+            {
+                if (player.Remove(11))
+                {
+                    player.Add(1);
+                    return true;
+                }
+                return false;
+            }
+            return true;
+        }
+        public bool checkBJ()
+        {
+            int value = playerValue();
+            if(value == 21)
+            {
+                return true;
+            }
+            return false;
+        }
+        public int playerValue()
+        {
+            int value = 0;
+            for (int i = 0; i < player.Count; i++)
+            {
+                value += player[i];
+            }
+            return value;
+        }
+        public int dealerValue()
+        {
+            int value = 0;
+            for (int i = 0; i < dealer.Count; i++)
+            {
+                value += dealer[i];
+            }
+            return value;
+        }
+        public void reset()
+        {
+            deckReset();
+            player.Clear();
+            dealer.Clear();
+        }
+        public int compare()
+        {
+            if (dealerValue() == playerValue())
+            {
+                return 0;
+            }
+            else if (dealerValue() < playerValue())
+            {
+                return 1;
+            }
+            else return 2;
+        }
     }
 }
-*/
-
-/*
-// När man väljer om Ess ska vara 1 eller 11
-string Ess;
-
-if (Console.ReadLine(Ess))
-{
-    Console.WriteLine("Vill du Ess ska vara 1 eller 11?");
-    if (Console.ReadLine("1"))
-    {
-    //read string Ess sedan tar den och convert till int och tar Ess och då blir Essets värde 1
-        Convert.ToInt32(string Ess);
-        int Ess = 1;
-    }
-    else if (Console.ReadLine("11"))
-    {
-        Convert.ToInt32(string Ess);
-        int Ess = 11;
-    }
-    Console.WriteLine(Ess)
-}
-//B
-*/
